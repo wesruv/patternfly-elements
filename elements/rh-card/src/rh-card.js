5adbfd23f8f24d51c60659a11685ced8f78afc0f
Michael Clayton <mclayton@redhat.com>
2018-07-13 14:02:36 -0400

    remove dead code from rh-card

--- elements/rh-card/src/rh-card.js
+++ elements/rh-card/src/rh-card.js
@@ -11,45 +11,6 @@ class RhCard extends Rhelement {
 
   connectedCallback() {
     super.connectedCallback();
-
-    // this.button = this.shadowRoot.querySelector("button");
-    //
-    // const child = this.children[0];
-    // let isHeadingTag = false;
-    //
-    // if (child) {
-    //   switch (child.tagName) {
-    //     case "H1":
-    //     case "H2":
-    //     case "H3":
-    //     case "H4":
-    //     case "H5":
-    //     case "H6":
-    //       isHeadingTag = true;
-    //       break;
-    //   }
-    //
-    //   const wrapperTag = document.createElement(child.tagName);
-    //   this.button.innerText = child.innerText;
-    //
-    //   wrapperTag.appendChild(this.button);
-    //   this.shadowRoot.appendChild(wrapperTag);
-    // } else {
-    //   this.button.innerText = this.textContent.trim();
-    // }
-    //
-    // if (!isHeadingTag) {
-    //   console.warn(
-    //     "The first child in the light DOM must be a heading level tag (h1, h2, h3, h4, h5, or h6)"
-    //   );
-    // }
-  }
-
-  disconnectedCallback() {}
-
-  attributeChangedCallback(attr, oldVal, newVal) {
-    if (attr === "theme") {
-    }
   }
 }
 
