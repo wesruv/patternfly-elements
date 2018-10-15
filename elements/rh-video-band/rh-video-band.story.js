dafd2c518e03d0c22e22c6e45c8c5b5dcc980580
Cas Roberts <carobert@redhat.com>
2018-10-15 13:06:25 -0400

    Generate template for rh-video-band

---
+++ elements/rh-video-band/rh-video-band.story.js
@@ -0,0 +1,11 @@
+import { storiesOf } from "@storybook/polymer";
+import "./rh-video-band";
+
+storiesOf("Video", module).add(
+  "rh-video-band",
+  () => `
+  <rh-video-band>
+    Video
+  </rh-video-band>
+  `
+);
