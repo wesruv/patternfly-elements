4175f72c33a5b5f408ad9463d4e4fd5ebe79bba3
starryeyez024 <kendalltotten@gmail.com>
2018-10-24 10:40:38 -0400

    WIP rh-video

--- elements/rh-video/rh-video.js
+++ elements/rh-video/rh-video.js
@@ -26,13 +26,99 @@ class RhVideo extends RHElement {
   get html() {
     return `
 <style>
+.rh-video-band {
+  position: relative;
+  padding-top: calc( var(--rh-theme--container-spacer, 1rem) * 4);
+  padding-bottom: calc( var(--rh-theme--container-spacer, 1rem) * 4); }
+
+.rh-video-band-container {
+  margin: 0 auto;
+  padding: 0 15px; }
+  @media (min-width: 768px) {
+    .rh-video-band-container {
+      padding: 0;
+      width: calc(var(--rh-theme--grid-breakpoint--sm, 576px) - 60px); } }
+  @media (min-width: 992px) {
+    .rh-video-band-container {
+      width: calc(var(--rh-theme--grid-breakpoint--md, 768px) - 60px); } }
+  @media (min-width: 1200px) {
+    .rh-video-band-container {
+      width: calc(var(--rh-theme--grid-breakpoint--lg, 992px) - 60px); } }
+
 :host {
   display: block; }
 
 :host([hidden]) {
   display: none; }
+
+:host {
+  background: gray; }
+
+.rh-video-band-embed {
+  position: relative;
+  width: 90%; }
+  @media (min-width: var(--rh-theme--grid-breakpoint--xs, 0)) {
+    .rh-video-band-embed {
+      width: 100%; } }
+
+@keyframes growBand {
+  0% {
+    opacity: 1;
+    z-index: 1;
+    min-height: 0; }
+  50% {
+    opacity: 0;
+    z-index: 1;
+    min-height: 550px; }
+  100% {
+    opacity: 0;
+    z-index: -1;
+    min-height: 550px; } }
+
+@keyframes shrinkBand {
+  0% {
+    opacity: 1;
+    z-index: 1;
+    max-height: 550px; }
+  50% {
+    opacity: 0;
+    z-index: -1;
+    max-height: 550px; }
+  100% {
+    opacity: 0;
+    z-index: -1;
+    max-height: 50px; } }
+
+@keyframes opacity {
+  50% {
+    opacity: 0;
+    z-index: -1; }
+  100% {
+    opacity: 1;
+    z-index: 1; } }
 </style>
-<slot></slot>`;
+<section class="rh-video-band" data-rh-theme="dark" data-rh-align="both" data-rh-band-grow="both">
+  <div class="rh-video-band-container" data-rh-layout="9" data-rh-align="horizontal">
+     <div class="rh-video-band-embed" data-rh-text-align="center">
+       <div class="rh-video-band-embed-details" data-rh-state="visible">
+         <slot name="video-play-icon">
+          <div class="rh-cta">
+            <a class="rh-cta-link" data-rh-cta-type="video-play" data-rh-action="video-play" href="//www.youtube.com/watch?4qCabv819Iw">The
+              Red Hat story</a>
+          </div>
+        </slot>
+       </div>
+       <div class="rh-video-embed-details" data-rh-state="hidden">
+        <div class="rh-video-embed-media-container">
+          <slot class="rh-video-embed-iframe-container" name="video"></slot>
+        </div>
+         <div class="rh-cta">
+          <a class="rh-cta-link" data-rh-cta-type="ghost" data-rh-action="video-close">close</a>
+      </div>
+       </div>
+    </div>
+   </div>
+</section>`;
   }
 
   static get tag() {
